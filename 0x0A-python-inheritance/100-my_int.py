249a0cc491da666d645a91ae21e4da0b22020960
olubunday <adeolaolumide9@gmail.com>
2023-04-11 13:04:09 -0700

    Correction

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -6,10 +6,10 @@
 class MyInt(int):
     """Invert int operators == and !=."""
 
-    def __eq__(self, value):
-        """Override == opeartor with != behavior."""
+    def __equal__(self, value):
+        """Override == operator with != behavior."""
         return self.real != value
 
-    def __ne__(self, value):
+    def __negate__(self, value):
         """Override != operator with == behavior."""
-        return self.real == 
+        return self.real == value

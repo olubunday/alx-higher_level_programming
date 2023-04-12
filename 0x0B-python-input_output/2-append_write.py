ff24fb1815cd5723743f31469fead8df067157c7
olubunday <adeolaolumide9@gmail.com>
2023-04-12 07:41:36 -0700

    Python I/O

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Contains the function append_write that appends a string"""
+
+
+def append_write(filename="", text=""):
+    """eturns the number of characters added:"""
+    with open(filename, 'a', encoding='utf=8') as f:
+        return f.write(text)
---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Contains the function load_from_json_file that creates an object from a “JSON file”"""
+
+import json
+
+
+def load_from_json_file(filename):
+    """crate and object from a "JSON file" """
+    with open(filename, 'r', encoding='utf-8') as f:
+        return json.load(f)

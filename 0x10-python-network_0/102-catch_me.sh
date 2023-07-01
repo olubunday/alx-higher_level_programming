68cca5fcb796eddead627ffe3c691083efccee18
olubunday <adeolaolumide9@gmail.com>
2023-07-01 06:49:53 -0700

    Corrected

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that makes a request to 0.0.0.0:5000/catch_me
-curl -s -X PUT -d "user_id=98" "http://0.0.0.0:5000/catch_me" -o /dev/null -w "You got me!"
+curl -s -X PUT -d "user_id=98" "http://0.0.0.0:5000/catch_me"
